9444ec82e2a6b3c07325983a23d82c90d3c8e164
Mark <Mark@b.com>
2023-07-01 16:47:39 +0800

    hide useless code

--- docs/sw copy.js
+++ docs/sw copy.js
@@ -1,14 +1,14 @@
-self.addEventListener("install", (e) => {
-  e.waitUntil(
-    caches
-      .open("nfcat-store")
-      .then((cache) => cache.addAll(["index.html", "script.js"]))
-  );
-});
+// self.addEventListener("install", (e) => {
+//   e.waitUntil(
+//     caches
+//       .open("nfcat-store")
+//       .then((cache) => cache.addAll(["index.html", "script.js"]))
+//   );
+// });
 
-self.addEventListener("fetch", (e) => {
-  alert(e.request.url);
-  e.respondWith(
-    caches.match(e.request).then((response) => response || fetch(e.request))
-  );
-});
+// self.addEventListener("fetch", (e) => {
+//   alert(e.request.url);
+//   e.respondWith(
+//     caches.match(e.request).then((response) => response || fetch(e.request))
+//   );
+// });
